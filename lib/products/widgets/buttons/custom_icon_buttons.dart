21ed473b8c17d889f90e78b1525ec6717ca422fb
Omer Erdem Aydemir <aydemirerdemomer@gmail.com>
2022-03-23 21:30:27 +0300

    custom buttons created

---
+++ lib/products/widgets/buttons/custom_icon_buttons.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomIconButton extends StatelessWidget {
+  final double height;
+  final double width;
+  final VoidCallback onPressed;
+  final Color color;
+  final Image icon;
+
+  CustomIconButton(
+      {Key? key,
+      required this.height,
+      required this.width,
+      required this.onPressed,
+      required this.color,
+      required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: icon,
+        color: color,
+      ),
+    );
+  }
+}
